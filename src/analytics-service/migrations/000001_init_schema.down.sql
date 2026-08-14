3393caa75b8ff9c815c888d4492b581ec76ad2f1
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-08-14 08:25:28 +0530

    feat(analytics): add analytics_db rollback migration

---
+++ src/analytics-service/migrations/000001_init_schema.down.sql
@@ -0,0 +1,4 @@
+-- src/analytics-service/migrations/000001_init_schema.down.sql
+DROP TABLE IF EXISTS executive_metrics;
+DROP TABLE IF EXISTS daily_cashflow;
+DROP TABLE IF EXISTS spending_aggregations;
