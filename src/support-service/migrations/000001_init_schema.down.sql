7d650e3586cc3c217de817950bd86a4acd5e54a7
Konda Balaji Rao <balajikonda9046@gmail.com>
2026-08-14 08:50:10 +0530

    feat(support): add migration down SQL for support_db

--- src/support-service/migrations/000001_init_schema.down.sql
+++ src/support-service/migrations/000001_init_schema.down.sql
@@ -1,6 +1,4 @@
 -- src/support-service/migrations/000001_init_schema.down.sql
 DROP TABLE IF EXISTS chat_messages;
-DROP TABLE IF EXISTS faqs;
-DROP TABLE IF EXISTS feedback;
 DROP TABLE IF EXISTS ticket_comments;
 DROP TABLE IF EXISTS tickets;
